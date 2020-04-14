158458ae061ce66b0368c8ccc9ae7a4ed9564f2d
Imran Kabir <imkabir13@hotmail.com>
2020-04-13 21:34:15 -0700

    Checking Jenkins server.

---
+++ src/test/java/mobileAppTest/CheckJenkins.java
@@ -0,0 +1,12 @@
+package mobileAppTest;
+
+import org.testng.annotations.Test;
+
+public class CheckJenkins
+{
+    @Test
+    public void jenkinsTest()
+    {
+        System.out.println("Hay Imran..! The Jenkins is working fine. . . .!!!");
+    }
+}
